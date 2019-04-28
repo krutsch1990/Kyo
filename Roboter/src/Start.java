8dcca5005bd3aff99d01dcffee0a39818c49fc41
Kyo <Kyo@192.168.0.5>
2019-04-29 00:01:18 +0200

    27.04.2019

--- Roboter/src/Start.java
+++ Roboter/src/Start.java
@@ -7,12 +7,16 @@ public class Start {
 
 	static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
 	static UnregulatedMotor motorC = new UnregulatedMotor(MotorPort.C);
-//	static TouchSensor touch= new TouchSensor(SensorPort.S1);
+	// static TouchSensor touch= new TouchSensor(SensorPort.S1);
 
 	public static void main(String[] args) {
 
+		// Aus der Klasse Color wird ein Objekt namens Color erzeugt.
 		Color color = new Color();
 
+		// Start des Programms, es wird mit color.brightness jeweils die Helligkeit vom
+		// Tisch
+		// und von der Linie gemessen und in einen float abgespeichert
 		Sound.beepSequence();
 		System.out.println("Press any button");
 		Button.waitForAnyPress();
@@ -32,72 +36,71 @@ public class Start {
 		Button.waitForAnyPress();
 		System.out.println("i am driving");
 
-		float error_count = 0;
+		int error_count = 0;
 		float error_sum = 0;
-		float error_average = 0;
+		float error_average = 1;
+		int speed_int_right;
+		int speed_int_left;
 
 		while (Button.ESCAPE.isUp()) {
-			++error_count;
 
-			if (error_count > 1000) {
+			// reset bei 10000 Messungen
+			if (error_count > 10000) {
 				error_count = 0;
 				error_sum = 0;
 			}
 
-			// System.out.println( color.brightness() );
-			System.out.println(error_average);
 			float actually = color.brightness();
 
 			if (actually < mid) {
 
-				int speed_int_right;
-
-				float min_error = linie;
+				float basis = linie;
 				float max_error = mid - linie;
+				float faktor = (actually - basis) / max_error;
+				float error = (max_error - (actually - max_error)) / max_error;
 
-				float actually_error = (actually - min_error) / max_error;
-				error_sum += actually_error;
 				error_average = error_sum / error_count;
 
-				float error_check = error_average / actually_error;
+				float error_check = error / error_average;
 
 				if (error_check > 2) {
-					speed_int_right = -15;
+					speed_int_right = -10;
 				} else {
-
-					float speed = actually_error * speed_max;
+					++error_count;
+					error_sum += error;
+					float speed = faktor * speed_max;
 					speed_int_right = (int) speed;
 				}
-
 				motorB.setPower(speed_max);
 				motorC.setPower(speed_int_right);
 
 			} else if (actually > mid) {
-				int speed_int_left;
 
-				float min_error = mid;
+				float basis = mid;
 				float max_error = tisch - mid;
+				float faktor = (actually - basis) / max_error;
+				float error = 1 - ((max_error - (actually - max_error)) / max_error);
 
-				float actually_error = (actually - min_error) / max_error;
-				error_sum += actually_error;
 				error_average = error_sum / error_count;
 
-				float error_check = error_average / actually_error;
+				float error_check = error / error_average;
 
-				if (error_check < 2) {
-					speed_int_left = -15;
+				if (error_check > 2) {
+					speed_int_left = -10;
 				} else {
-					float speed = speed_max - (actually * speed_max);
+					++error_count;
+					error_sum += error;
+					float speed = speed_max - (faktor * speed_max);
 					speed_int_left = (int) speed;
 				}
 
 				motorC.setPower(speed_max);
 				motorB.setPower(speed_int_left);
+
 			} else {
 				motorC.setPower(speed_max);
 				motorB.setPower(speed_max);
 			}
-
 		}
 
 		motorB.stop();
